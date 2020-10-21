befe9d8ed33f977400129daf22c2a19de266d7aa
Eduardo Pinheiro <3dup1n@gmail.com>
2020-10-21 13:44:16 -0300

    index refactored

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import bg from "../../assets/img/bg-home.jpg";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class Home extends React.Component {
   test() {
@@ -8,25 +7,20 @@ class Home extends React.Component {
   }
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/">
-            <div
-              style={{
-                height: `calc(100vh - 95px)`,
-                backgroundImage: `url(${bg})`,
-                backgroundSize: `calc(100%) calc(100%)`,
-                backgroundRepeat: "no-repeat",
-                zIndex: 1,
-              }}
-            >
-              <h1 onMouseEnter={this.test} className="page-header">
-                Bons Drinks
-              </h1>
-            </div>
-          </Route>
-        </Switch>
-      </Router>
+      <div
+        style={{
+          height: `calc(100vh - 95px)`,
+          backgroundImage: `url(${bg})`,
+          backgroundSize: `calc(100%) calc(100%)`,
+          backgroundRepeat: "no-repeat",
+          zIndex: 1,
+          textShadow: `5px 5px 5px black`,
+        }}
+      >
+        <h1 onMouseEnter={this.test} className="page-header">
+          Bons Drinks
+        </h1>
+      </div>
     );
   }
 }
